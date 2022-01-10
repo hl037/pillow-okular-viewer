e943cc938aea33ff600c49f6e03062d904e127a1
Léo Flaventin Hauchecorne <hl037.prog@gmail.com>
2022-01-11 00:00:03 +0100

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+A simple package to make okular the default viewer with PIL
+
+# Usage
+
+Simply `import pillowOkularViewer`, And okular is you viewer
---
+++ pillowOkularViewer/__init__.py
@@ -0,0 +1,18 @@
+from PIL.ImageShow import Viewer, register
+
+class OkularViewer(Viewer):
+  """
+  Okular image viewer for PIL
+  """
+
+  def get_command(self, file, title=None, **options):
+    if title :
+      return f'okular --title "{title}" "{file}"'
+    return f'okular {file}'
+
+register(OkularViewer(), order=0)
+    
+
+
+
+
